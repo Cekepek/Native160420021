0b69c56686cea044c445b6e8bc2896cc2cadaa57
StevenLabina <stevenlabina25@gmail.com>
2023-03-01 11:34:15 +0700

    dr steven

--- app/src/main/java/com/cekepek/native160420021/MainActivity.kt
+++ app/src/main/java/com/cekepek/native160420021/MainActivity.kt
@@ -2,10 +2,13 @@ package com.cekepek.native160420021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var test = "HALO"
     }
 }
